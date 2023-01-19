83f6d2496c07ab566c3fb027f67e20798f7b2f3a
allnat28 <nate.allbee@hotmail.com>
2023-01-19 16:03:27 -0600

    Initial Commit

---
+++ Shared/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  Shared
+//
+//  Created by admin on 1/19/23.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
